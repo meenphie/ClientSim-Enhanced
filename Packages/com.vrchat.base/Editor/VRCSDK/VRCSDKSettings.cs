c10db311b8ea84f7aa174fd06628259df7853815
Fairplex <fairplexvrc@gmail.com>
2024-11-04 22:17:30 +0100

    Initial Commit

---
+++ Packages/com.vrchat.base/Editor/VRCSDK/VRCSDKSettings.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace VRC.SDKBase.Editor
+{
+    public static class VRCSDKSettingsProvider
+    {
+        public static readonly string customSettingsPath = "VRChat/SDK";
+
+        [SettingsProvider]
+        public static SettingsProvider CreateVRCSDKSettingsProvider()
+        {
+            var provider = new SettingsProvider(customSettingsPath, SettingsScope.Project)
+            {
+                label = "SDK",
+                
+                // This method will draw the UI for the settings
+                activateHandler = (searchContext, rootElement) =>
+                {
+                    var styleSheet = Resources.Load<StyleSheet>("VRCSDKSettingsStyle");
+                    rootElement.styleSheets.Add(styleSheet);
+
+                    var container = new VisualElement()
+                    {
+                        name = "container",
+                    };
+                    rootElement.Add(container);
+
+                    var titleLabel = new Label("VRChat SDK Settings")
+                    {
+                        name = "title-label"
+                    };
+                    
+                    container.Add(titleLabel);
+
+                    var androidAppNameField = new UnityEngine.UIElements.TextField("Android App Package Name")
+                    {
+                        value = VRCSettings.AndroidPackageName,
+                        name = "android-app-package-name-field"
+                    };
+                    androidAppNameField.RegisterValueChangedCallback(evt =>
+                    {
+                        VRCSettings.AndroidPackageName = evt.newValue;
+                    });
+                    container.Add(androidAppNameField);
+                },
+
+                // Populate the search keywords to enable smart search filtering and label highlighting
+                keywords = new[] { "vrchat", "sdk", "mobile", "android" }
+            };
+
+            return provider;
+        }
+        
+        public static void OpenSettings()
+        {
+            // Open the Project Settings window with these settings selected
+            SettingsService.OpenProjectSettings(customSettingsPath);
+        }
+    }
+}
