45bc398610a0a16b5c4555e824381eb1ed49a180
Fairplex <fairplexvrc@gmail.com>
2025-03-02 23:47:02 +0100

    Removed mouse movement in VR

--- Packages/com.vrchat.worlds/Integrations/ClientSim/Runtime/Input/ClientSimInputActionBased.cs
+++ Packages/com.vrchat.worlds/Integrations/ClientSim/Runtime/Input/ClientSimInputActionBased.cs
@@ -15,7 +15,7 @@ namespace VRC.SDK3.ClientSim
     // right after modifying the input device, which will happen at the end of the frame instead. In order to properly
     // process input events the same in runtime and tests, the events will need to be queued and processed in mono behaviours.
     // This would also allow for other event types to be included that does not use the Input System (eg vr controls).
-    
+
     /// <summary>
     /// An implementation of ClientSimInput that receives input events from Unity's Input System using InputActions.
     /// Events from the action system are then sent to systems listening to input events.
@@ -23,15 +23,15 @@ namespace VRC.SDK3.ClientSim
     public class ClientSimInputActionBased : ClientSimInputBase
     {
         private readonly ClientSimSettings _settings;
-        
-/* Items need to be wrapped in this define to prevent compiler errors on initial import
- when the Input System has not yet been imported or enabled.*/
+
+        /* Items need to be wrapped in this define to prevent compiler errors on initial import
+         when the Input System has not yet been imported or enabled.*/
 #if ENABLE_INPUT_SYSTEM
         private readonly InputAction _movementHorizontal;
         private readonly InputAction _movementVertical;
         private readonly InputAction _lookHorizontal;
         private readonly InputAction _lookVertical;
-        
+
         private readonly InputAction _jumpLeft;
         private readonly InputAction _jumpRight;
         private readonly InputAction _useLeft;
@@ -42,7 +42,7 @@ namespace VRC.SDK3.ClientSim
         private readonly InputAction _dropRight;
         private readonly InputAction _toggleMenuLeft;
         private readonly InputAction _toggleMenuRight;
-        
+
         #region NonVR Only
 
         private readonly InputAction _run;
@@ -58,18 +58,18 @@ namespace VRC.SDK3.ClientSim
         #endregion
 
         private InputActionAsset InputActions;
-        
+
         public ClientSimInputActionBased(InputActionAsset actionAsset, ClientSimSettings settings)
         {
             InputActions = actionAsset;
-            
+
             _settings = settings;
-            
+
             _lookHorizontal = actionAsset["LookHorizontal"];
             _lookVertical = actionAsset["LookVertical"];
             _movementHorizontal = actionAsset["MovementHorizontal"];
             _movementVertical = actionAsset["MovementVertical"];
-            
+
             _jumpLeft = actionAsset["JumpLeft"];
             _jumpRight = actionAsset["JumpRight"];
             _useLeft = actionAsset["UseLeft"];
@@ -80,7 +80,7 @@ namespace VRC.SDK3.ClientSim
             _dropRight = actionAsset["DropRight"];
             _toggleMenuLeft = actionAsset["ToggleMenuLeft"];
             _toggleMenuRight = actionAsset["ToggleMenuRight"];
-            
+
             // Desktop only input options.
             _run = actionAsset["Run"];
             _toggleCrouch = actionAsset["ToggleCrouch"];
@@ -97,22 +97,22 @@ namespace VRC.SDK3.ClientSim
             _jumpLeft.canceled += HandleJumpLeft;
             _jumpRight.performed += HandleJumpRight;
             _jumpRight.canceled += HandleJumpRight;
-            
+
             _useLeft.performed += HandleUseLeft;
             _useLeft.canceled += HandleUseLeft;
             _useRight.performed += HandleUseRight;
             _useRight.canceled += HandleUseRight;
-            
+
             _grabLeft.performed += HandleGrabLeft;
             _grabLeft.canceled += HandleGrabLeft;
             _grabRight.performed += HandleGrabRight;
             _grabRight.canceled += HandleGrabRight;
-            
+
             _dropLeft.performed += HandleDropLeft;
             _dropLeft.canceled += HandleDropLeft;
             _dropRight.performed += HandleDropRight;
             _dropRight.canceled += HandleDropRight;
-            
+
             _toggleMenuLeft.performed += HandleToggleMenuLeft;
             _toggleMenuLeft.canceled += HandleToggleMenuLeft;
             _toggleMenuRight.performed += HandleToggleMenuRight;
@@ -126,13 +126,13 @@ namespace VRC.SDK3.ClientSim
             _toggleProne.canceled += HandleToggleProne;
             _releaseMouse.performed += HandleReleaseMouse;
             _releaseMouse.canceled += HandleReleaseMouse;
-            
+
             foreach (InputAction action in actionAsset)
             {
                 action.performed += SetInputDevice;
             }
         }
-        
+
         private VRCInputMethod _lastInputMethod = VRCInputMethod.Count;
 
         public VRCInputMethod LastInputMethod
@@ -141,8 +141,8 @@ namespace VRC.SDK3.ClientSim
             set
             {
                 // Only send events for changes
-                if(_lastInputMethod == value) return;
-                
+                if (_lastInputMethod == value) return;
+
                 _lastInputMethod = value;
                 SendInputMethodChangedEvent(_lastInputMethod);
             }
@@ -152,7 +152,7 @@ namespace VRC.SDK3.ClientSim
         bool isTouchActive = false;
         private void SetInputDevice(InputAction.CallbackContext ctx)
         {
-            if(Regex.IsMatch(ctx.action.ToString(), MOUSE_LOOK_PATTERN)) return; // This is to avoid changing the tooltip icon when looking with a mouse
+            if (Regex.IsMatch(ctx.action.ToString(), MOUSE_LOOK_PATTERN)) return; // This is to avoid changing the tooltip icon when looking with a mouse
             isTouchActive = (ctx.control.device.description.empty || Touchscreen.current != null && ctx.control.device.GetType().Equals(Touchscreen.current.GetType())); // On screen controls
             if (!isTouchActive)
             {
@@ -187,28 +187,28 @@ namespace VRC.SDK3.ClientSim
         public override void Dispose()
         {
             base.Dispose();
-            
+
             // Go through and unsubscribe from all input actions
             _jumpLeft.performed -= HandleJumpLeft;
             _jumpLeft.canceled -= HandleJumpLeft;
             _jumpRight.performed -= HandleJumpRight;
             _jumpRight.canceled -= HandleJumpRight;
-            
+
             _useLeft.performed -= HandleUseLeft;
             _useLeft.canceled -= HandleUseLeft;
             _useRight.performed -= HandleUseRight;
             _useRight.canceled -= HandleUseRight;
-            
+
             _grabLeft.performed -= HandleGrabLeft;
             _grabLeft.canceled -= HandleGrabLeft;
             _grabRight.performed -= HandleGrabRight;
             _grabRight.canceled -= HandleGrabRight;
-            
+
             _dropLeft.performed -= HandleDropLeft;
             _dropLeft.canceled -= HandleDropLeft;
             _dropRight.performed -= HandleDropRight;
             _dropRight.canceled -= HandleDropRight;
-            
+
             _toggleMenuLeft.performed -= HandleToggleMenuLeft;
             _toggleMenuLeft.canceled -= HandleToggleMenuLeft;
             _toggleMenuRight.performed -= HandleToggleMenuRight;
@@ -229,73 +229,73 @@ namespace VRC.SDK3.ClientSim
         {
             SendJumpEvent(context.ReadValueAsButton(), HandType.LEFT);
         }
-        
+
         private void HandleJumpRight(InputAction.CallbackContext context)
         {
             SendJumpEvent(context.ReadValueAsButton(), HandType.RIGHT);
         }
-        
+
         private void HandleUseLeft(InputAction.CallbackContext context)
         {
             SendUseEvent(context.ReadValueAsButton(), HandType.LEFT);
         }
-        
+
         private void HandleUseRight(InputAction.CallbackContext context)
         {
             SendUseEvent(context.ReadValueAsButton(), HandType.RIGHT);
         }
-        
+
         private void HandleGrabLeft(InputAction.CallbackContext context)
         {
             SendGrabEvent(context.ReadValueAsButton(), HandType.LEFT);
         }
-        
+
         private void HandleGrabRight(InputAction.CallbackContext context)
         {
             SendGrabEvent(context.ReadValueAsButton(), HandType.RIGHT);
         }
-        
+
         private void HandleDropLeft(InputAction.CallbackContext context)
         {
             SendDropEvent(context.ReadValueAsButton(), HandType.LEFT);
         }
-        
+
         private void HandleDropRight(InputAction.CallbackContext context)
         {
             SendDropEvent(context.ReadValueAsButton(), HandType.RIGHT);
         }
-        
+
         private void HandleToggleMenuLeft(InputAction.CallbackContext context)
         {
             SendToggleMenuEvent(context.ReadValueAsButton(), HandType.LEFT);
         }
-        
+
         private void HandleToggleMenuRight(InputAction.CallbackContext context)
         {
             SendToggleMenuEvent(context.ReadValueAsButton(), HandType.RIGHT);
         }
-        
+
         private void HandleRun(InputAction.CallbackContext context)
         {
             SendRunEvent(context.ReadValueAsButton());
         }
-        
+
         private void HandleToggleCrouch(InputAction.CallbackContext context)
         {
             SendToggleCrouchEvent(context.ReadValueAsButton());
         }
-        
+
         private void HandleToggleProne(InputAction.CallbackContext context)
         {
             SendToggleProneEvent(context.ReadValueAsButton());
         }
-        
+
         private void HandleReleaseMouse(InputAction.CallbackContext context)
         {
             SendReleaseMouseEvent(context.ReadValueAsButton());
         }
 #endif
-        
+
         public override float GetMovementHorizontal()
         {
 #if ENABLE_INPUT_SYSTEM
@@ -304,7 +304,7 @@ namespace VRC.SDK3.ClientSim
             return 0;
 #endif
         }
-        
+
         public override float GetMovementVertical()
         {
 #if ENABLE_INPUT_SYSTEM
@@ -313,11 +313,15 @@ namespace VRC.SDK3.ClientSim
             return 0;
 #endif
         }
-        
+
         public override float GetLookHorizontal()
         {
 #if ENABLE_INPUT_SYSTEM
             if (_pickupRotateLeftRight.ReadValue<float>() != 0)
+            {
+                return 0;
+            }
+            if (_settings._enableVRMode)
             {
                 return 0;
             }
@@ -326,11 +330,15 @@ namespace VRC.SDK3.ClientSim
             return 0;
 #endif
         }
-        
+
         public override float GetLookVertical()
         {
 #if ENABLE_INPUT_SYSTEM
             if (_pickupRotateUpDown.ReadValue<float>() != 0)
+            {
+                return 0;
+            }
+            if (_settings._enableVRMode)
             {
                 return 0;
             }
@@ -357,7 +365,7 @@ namespace VRC.SDK3.ClientSim
             return 0;
 #endif
         }
-        
+
         public override float GetPickupRotateCwCcw()
         {
 #if ENABLE_INPUT_SYSTEM
@@ -366,7 +374,7 @@ namespace VRC.SDK3.ClientSim
             return 0;
 #endif
         }
-        
+
         public override float GetPickupManipulateDistance()
         {
 #if ENABLE_INPUT_SYSTEM
--- Packages/com.vrchat.worlds/Integrations/ClientSim/Runtime/Player/ClientSimPlayerController.cs
+++ Packages/com.vrchat.worlds/Integrations/ClientSim/Runtime/Player/ClientSimPlayerController.cs
@@ -37,14 +37,14 @@ namespace VRC.SDK3.ClientSim
         private IClientSimEventDispatcher _eventDispatcher;
         private IClientSimInput _input;
         private IClientSimTrackingProvider _trackingProvider;
-        
+
         private CharacterController _characterController;
         private Transform _cameraProxyObject;
 
         private bool _isDead;
         private bool _isWalking = true; // Player defaults to walking
         private bool _jump;
-        
+
         private Vector2 _prevInput;
         // Check if the directionality changed to apply "stutter stepping" for legacy locomotion.
         private bool _directionChanged;
@@ -83,10 +83,10 @@ namespace VRC.SDK3.ClientSim
             _stationManager = stationManager;
 
             _cameraProxyObject = proxyProvider.CameraProxy().transform;
-            
+
             Subscribe();
         }
-        
+
         private void Start()
         {
             NotifyPlayerMoved();
@@ -96,7 +96,7 @@ namespace VRC.SDK3.ClientSim
         {
             Dispose();
         }
-        
+
         private void Subscribe()
         {
             // Input will be null with incorrect Unity input project settings.
@@ -109,7 +109,7 @@ namespace VRC.SDK3.ClientSim
             _eventDispatcher.Subscribe<ClientSimPlayerDeathStatusChangedEvent>(CombatStatusEvent);
             _eventDispatcher.Subscribe<ClientSimOnTrackingScaleUpdateEvent>(OnTrackingScaleUpdated);
         }
-        
+
         public void Dispose()
         {
             _input?.UnsubscribeJump(JumpInput);
@@ -151,17 +151,17 @@ namespace VRC.SDK3.ClientSim
         {
             _menuIsOpen = stateChangedEvent.isMenuOpen;
         }
-        
+
         private void MenuRespawnEvent(ClientSimMenuRespawnClickedEvent stateChangedEvent)
         {
             Respawn();
         }
-        
+
         private void MouseReleasedEvent(ClientSimMouseReleasedEvent mouseReleasedEvent)
         {
             _mouseReleased = mouseReleasedEvent.isReleased;
         }
-        
+
         private void CombatStatusEvent(ClientSimPlayerDeathStatusChangedEvent combatStatusEvent)
         {
             _isDead = combatStatusEvent.isDead;
@@ -173,7 +173,7 @@ namespace VRC.SDK3.ClientSim
         }
 
         #endregion
-        
+
         public Vector3 GetPosition()
         {
             return transform.position;
@@ -188,7 +188,7 @@ namespace VRC.SDK3.ClientSim
         {
             return _characterController.velocity;
         }
-        
+
         public void SetVelocity(Vector3 velocity)
         {
             _playerRetainedVelocity = velocity;
@@ -209,7 +209,7 @@ namespace VRC.SDK3.ClientSim
 
         public void Respawn(int index)
         {
-            
+
             Transform spawnPoint = _sceneManager.GetSpawnPoint(index);
             if (spawnPoint == null)
             {
@@ -217,7 +217,7 @@ namespace VRC.SDK3.ClientSim
                 spawnPoint = _sceneManager.GetSpawnPoint(0);
             }
             Teleport(spawnPoint, false);
-            _eventDispatcher.SendEvent(new ClientSimOnPlayerRespawnEvent { player =  _playerApi.Player });
+            _eventDispatcher.SendEvent(new ClientSimOnPlayerRespawnEvent { player = _playerApi.Player });
         }
 
         public void Teleport(Transform point, bool fromPlaySpace)
@@ -246,7 +246,7 @@ namespace VRC.SDK3.ClientSim
 
             NotifyPlayerMoved();
             _eventDispatcher.SendEvent(new ClientSimOnPlayerTeleportedEvent { player = _playerApi.Player });
-            
+
             Physics.SyncTransforms();
         }
 
@@ -271,10 +271,10 @@ namespace VRC.SDK3.ClientSim
             {
                 Teleport(station.ExitLocation(), false);
             }
-            
+
             _jump = false;
         }
-        
+
         public void SitPosition(Transform seat)
         {
             transform.SetPositionAndRotation(seat.position, seat.rotation);
@@ -290,12 +290,12 @@ namespace VRC.SDK3.ClientSim
             {
                 Respawn();
             }
-            
+
             GetInput();
 
             NotifyPlayerMoved();
         }
-   
+
         private void FixedUpdate()
         {
             Physics.SyncTransforms();
@@ -401,13 +401,16 @@ namespace VRC.SDK3.ClientSim
             // Only allow these input actions while the menu is closed
             //if (!_menuIsOpen)
             {
-                GetMovementInput();
-                GetSteamVRMovementInput();
-                RotateView();
-                SteamVRRotateView();
+
+                    GetSteamVRMovementInput();
+                    SteamVRRotateView();
+
+                    GetMovementInput();
+                    RotateView();
+                
             }
         }
-        
+
         private void GetMovementInput()
         {
             Vector2 input = _input.GetMovementAxes();
@@ -434,7 +437,7 @@ namespace VRC.SDK3.ClientSim
             _directionChanged = (input.sqrMagnitude < 1e-3 ^ _prevInput.sqrMagnitude < 1e-3);
             _prevInput = input;
         }
-        
+
         // TODO Move rotation of the player controller to be done in the tracking provider and have the player controller
         // copy the head rotation. This would allow more generic handling of mouse released, VR snap turning, and locked in station.
         private void RotateView()
@@ -447,7 +450,7 @@ namespace VRC.SDK3.ClientSim
                 transform.rotation *= Quaternion.Euler(0f, yRot, 0f);
             }
         }
-        
+
         private void SteamVRRotateView()
         {
             // Allow player controller to look left and right when not in a locked station and for desktop users
@@ -458,7 +461,7 @@ namespace VRC.SDK3.ClientSim
                 transform.rotation *= Quaternion.Euler(0f, yRot, 0f);
             }
         }
-        
+
         // Used in tests to help look a specific direction
         public void LookTowardsPoint(Vector3 point)
         {
@@ -484,14 +487,14 @@ namespace VRC.SDK3.ClientSim
             }
             var cameraTrackingData = _trackingProvider.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
             _cameraProxyObject.SetPositionAndRotation(cameraTrackingData.position, cameraTrackingData.rotation);
-            _cameraProxyObject.localScale = _trackingProvider.GetTrackingScale() * Vector3.one; 
+            _cameraProxyObject.localScale = _trackingProvider.GetTrackingScale() * Vector3.one;
         }
 
         private Vector2 GetSpeed()
         {
             // TODO check current bindings to see if non keyboard and only use runspeed.
             Vector2 speed = new Vector2(
-                _isWalking? _playerLocomotionData.GetWalkSpeed() : _playerLocomotionData.GetRunSpeed(),
+                _isWalking ? _playerLocomotionData.GetWalkSpeed() : _playerLocomotionData.GetRunSpeed(),
                 _playerLocomotionData.GetStrafeSpeed());
 
             switch (_trackingProvider.GetPlayerStance())
